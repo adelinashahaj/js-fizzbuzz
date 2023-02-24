fe2b4df252ed82425ebd864e0877d709a001b5b5
adelinashahaj <adelinarucaj@icloud.com>
2023-02-24 15:14:41 +0100

    fine bonus 1

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
-for (var i = 1; i <= 100; i++) {
 
-    if (i % 3 == 0 && i % 5 == 0) {
-      console.log('FizzBuzz');
-    } else if (i % 3 == 0) {
-      console.log('Fizz');
-    } else if (i % 5 == 0) {
-      console.log('Buzz');
-    } else {
-      console.log(i);
-    }
+
+  const containerDom = document.querySelector('.container');
   
-  }
+for (let i = 1; i <= 100; i++ ) {
+    const quadratino = document.createElement('div');
+    quadratino.classList.add('square');
+    quadratino.append(i);
+    containerDom.append(quadratino);
+     if (i % 3 == 0 && i % 5 == 0) {
+        console.log('FizzBuzz');
+      } else if (i % 3 == 0) {
+        console.log('Fizz');
+      } else if (i % 5 == 0) {
+        console.log('Buzz');
+      } else {
+        console.log(i);
+      }
+}
