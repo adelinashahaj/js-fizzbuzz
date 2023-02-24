e12e3741d755d3fccc6fc17793dcbc79c39ac0da
adelinashahaj <adelinarucaj@icloud.com>
2023-02-24 16:24:12 +0100

    esercizio concluso

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
 
   const containerDom = document.querySelector('.container');
-  
+ 
 for (let i = 1; i <= 100; i++ ) {
     const quadratino = document.createElement('div');
     quadratino.classList.add('square');
@@ -9,10 +9,16 @@ for (let i = 1; i <= 100; i++ ) {
     containerDom.append(quadratino);
      if (i % 3 == 0 && i % 5 == 0) {
         console.log('FizzBuzz');
+        quadratino.innerHTML = 'FizzBuzz';
+        quadratino.style.backgroundColor = "red";
       } else if (i % 3 == 0) {
         console.log('Fizz');
+        quadratino.innerHTML = 'frizz';
+        quadratino.style.backgroundColor = "yellow";
       } else if (i % 5 == 0) {
         console.log('Buzz');
+        quadratino.innerHTML = 'Buzz';
+        quadratino.style.backgroundColor = "green";
       } else {
         console.log(i);
       }
